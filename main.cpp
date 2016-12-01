d0c738eb6594b93b74f8056dc091bb803f9d80bf
= <=forhadmethun@gmail.com>
2016-12-01 20:45:08 +0600

    message

--- main.cpp
+++ main.cpp
@@ -2,33 +2,9 @@
 using namespace std;
 int main()
 {
-    //freopen("D:\\googleDrive\\_CSE\\URI\\in.txt","r",stdin);
-    string str1,str2;
-    int tc = 1;
-    //cout <<"test" <<endl;
-    int count = 0;
-    while(getline(cin,str1)){
+    //uri
+    freopen("D:\\googleDrive\\_CSE\\URI\\in.txt","r",stdin);
+//    freopen("D:\\googleDrive\\_CSE\\URI\\out.txt","w",stdout);
 
-        if(str1 == "0")break;
-        if(count ==0){
-            count++;
-        }
-        else{
-            cout << endl;
-        }
-
-        getline(cin,str2);
-        printf("Instancia %d\n",tc++);
-       // cout << "------------------> " << str2.find(str1) << endl;
-        if(str2.find(str1) != string::npos){
-            cout << "verdadeira" << endl;
-        }
-        else{
-            cout << "falsa" << endl;
-        }
-
-
-
-    }
     return 0;
 }
---
+++ unsolved/uri_1667.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //uri 1667
+    freopen("D:\\googleDrive\\_CSE\\URI\\in.txt","r",stdin);
+    freopen("D:\\googleDrive\\_CSE\\URI\\out.txt","w",stdout);
+    string str;
+    vector<string> vs;
+    while(getline(cin,str)){
+        stringstream ss(str);
+        string temp;
+        while(ss>>temp){
+            vs.push_back(temp);
+        }
+    }
+    bool firstLine = false;
+    int len = 0;
+    for(auto it= vs.begin();it!=vs.end();it++){
+        if(firstLine == false){
+            firstLine = true;
+        }
+        else{
+            cout << " ";
+            len++;
+        }
+        if((*it).length() + len >80){
+            cout << endl;
+            len = 0;
+            firstLine = false;
+        }
+
+        if((*it) == "<br>"){
+            cout << endl;
+            firstLine = false;
+            len = 0;
+        }
+       else if(*it == "<hr>"){
+            cout <<endl <<  "--------------------------------------------------------------------------------" << endl;
+            firstLine = false;
+            len = 0;
+        }
+        else {
+            cout << *it;
+            len+=(*it).length();
+        }
+    }
+
+    return 0;
+}

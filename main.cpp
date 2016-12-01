28324153b13532d64f1cf17d4236f8bd0f8a3345
= <=forhadmethun@gmail.com>
2016-12-01 20:11:54 +0600

    message

--- main.cpp
+++ main.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 int main()
 {
+    //freopen("D:\\googleDrive\\_CSE\\URI\\in.txt","r",stdin);
+    string str1,str2;
+    int tc = 1;
+    //cout <<"test" <<endl;
+    int count = 0;
+    while(getline(cin,str1)){
 
-    freopen("D:\\googleDrive\\_CSE\\_dayWise_work\\2016\\july_august_september_oct_nov_dec\\Oct\\URI_BEGINNER\\in.txt","r",stdin);
+        if(str1 == "0")break;
+        if(count ==0){
+            count++;
+        }
+        else{
+            cout << endl;
+        }
+
+        getline(cin,str2);
+        printf("Instancia %d\n",tc++);
+       // cout << "------------------> " << str2.find(str1) << endl;
+        if(str2.find(str1) != string::npos){
+            cout << "verdadeira" << endl;
+        }
+        else{
+            cout << "falsa" << endl;
+        }
+
+
+
+    }
     return 0;
 }

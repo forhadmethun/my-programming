203b33c4eddfb5d183e30ecc5c6f937fc3d6caf2
= <=forhadmethun@gmail.com>
2017-03-28 01:13:58 +0600

    message

--- DataStructure/Array/two_dimensional.cpp
+++ DataStructure/Array/two_dimensional.cpp
@@ -5,14 +5,21 @@ using namespace std;
  * a way of returing a  2d matrix from a function
  */
 
+
+//a function for creating a 2d matrix and return as int** type... :)
 int** create_2d_array(){
     int **array = new int*[10];
     for(int i=0;i<10;i++){
         array[i] = new int[10];
     }
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            array[i][j]  = 0;
+        }
+    }
     return array;
 }
-
+//multiplying two mtrix
 int** mul(int** mat1, int** mat2,int r1,int c1,int r2,int c2){
     int** mult = create_2d_array();
     for(int i = 0; i < r1; ++i)
@@ -31,7 +38,7 @@ int** mul(int** mat1, int** mat2,int r1,int c1,int r2,int c2){
     return mult;
 }
 
-
+//creating a multipication table with 2d matrix
 int **create_multipication_table(int** array){
     for(int i=1;i<=10;i++){
         for(int j=1;j<=10;j++){
@@ -40,6 +47,7 @@ int **create_multipication_table(int** array){
     }
     return array;
 }
+//check the multipication table
 void time_table(){
     int** array1 = create_2d_array();
     int** answer_array = create_multipication_table(array1);
@@ -50,6 +58,7 @@ void time_table(){
         printf("\n");
     }
 }
+//print the matrix
 void print_matrix(int** mat,int row,int col){
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
@@ -57,7 +66,9 @@ void print_matrix(int** mat,int row,int col){
         }
         printf("\n");
     }
+    printf("\n");
 }
+//input two matrix and check the mul() function
 void matrix_multipicatin(){
     //creating first matrix
     int** mult1 = create_2d_array();
@@ -88,25 +99,62 @@ void matrix_multipicatin(){
 //    mult2[1][1] = 2;
 //    mult2[2][2] = 3;
 //    mult2[3][3] = 4;
-
+    //initialize matrix two
     mult2[0][0] = 1;
     mult2[1][0] = 2;
     mult2[2][0] = 3;
     mult2[3][0] = 4;
 
 
-
+    //printing the content of the matrix
     print_matrix(mult1,4,4);
     printf("\n");
     print_matrix(mult2,4,1);
     printf("\n");
+
+
+    //multiplying the matrix
     mult = mul(mult1,mult2,4,4,4,1);
     print_matrix(mult,4,1);
 
 }
+
+//array of matrix
+void vector_of_matrix(){
+    int **mat = create_2d_array();
+    mat[0][0] = 1;
+    vector< int** > vi;
+    vi.push_back(mat);
+    mat = create_2d_array();
+    mat[0][0] = 2;
+    vi.push_back(mat);
+
+    for(int i=0;i<vi.size();i++){
+        print_matrix(vi[i],4,4);
+        printf("\n");
+    }
+
+}
+//we should clear the memory after creating...2d matrix with new operator
+void delete_memory(int** mat,int row){
+    for(int i=0;i<row;i++){
+        delete[] mat[i];
+        printf("freeing memory\n");
+    }
+
+//    int **mat = create_2d_array();
+//    print_matrix(mat,10,10);
+//    delete_memory(mat,10);
+//    print_matrix(mat,10,10);
+}
+
+//for testing all the functions
 int main(){
     //time_table();
-    matrix_multipicatin();
+    //matrix_multipicatin();
+    //vector_of_matrix();
+
+
 
     return 0;
 }

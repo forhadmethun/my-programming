141587314e2d7697cd48d9fecd1174c82a733fc7
= <=forhadmethun@gmail.com>
2017-03-30 15:37:41 +0600

    message

--- Others/Graphics/Offline_2/main.cpp
+++ Others/Graphics/Offline_2/main.cpp
@@ -7,11 +7,29 @@ struct point{
         y = 0;
         z = 0;
     }
+    point(double px,double py,double pz){
+        x = px;
+        y = py;
+        z = pz;
+    }
+    operator = (point p){
+        x = p.x;
+        y = p.y;
+        z = p.z;
+    }
+
 };
 struct triangle{
     point a,b,c;
 };
 
+point R(point x,point a,double theta){
+    point p;
+    p.x = a.x * cos(theta) + (1-cos(theta))*(a.x*x.x + a.y *x.y + a.z * x.z)*a.x + sin(theta)*(a.y*x.z - x.y*a.z);
+    p.y = a.y * cos(theta) + (1-cos(theta))*(a.x*x.x + a.y *x.y + a.z *x.z) *a.y + sin(theta)*(a.z*x.x - a.x*x.z);
+    p.z = a.z * cos(theta) + (1-cos(theta))*(a.x*x.x + a.y *x.y + a.z *x.z) *a.z + sin(theta)*(a.x*x.y - a.y*x.x);
+    return p;
+}
 
 
 
@@ -158,7 +176,7 @@ int main(){
     char buff[255];
 
     fp1=fopen("D:\\googleDrive\\_CSE\\Code\\Others\\Graphics\\Offline_2\\scene.txt","r");
-    point eye,look_at,up,l,r,u;
+    point eye,look_at,up,l,r,u,a;
     double field_of_view, aspect_ratio, near, far;
     //cin >> eye.x >> eye.y >> eye.z >> look_at.x >> look_at.y >> look_at.z >> up.x >> up.y >> up.z;
     //input gluLookAt
@@ -255,9 +273,11 @@ int main(){
         }
         else if(input == "push"){
 
+
             continue;
         }
         else if(input == "pop"){
+
             continue;
         }
         else if(input == "scale"){
@@ -281,7 +301,51 @@ int main(){
         else if(input == "rotate"){
 
             //cin >> angle >> p.x >> p.y >> p.z;
-            fscanf(fp1,"%lf%lf%lf%lf\n",&angle,&p.x,&p.z,&p.z);
+            fscanf(fp1,"%lf%lf%lf%lf\n",&angle,&a.x,&a.y,&a.z);
+            //normalizing a
+            a.x = a.x / sqrt(a.x*a.x + a.y*a.y + a.z *a.z);
+            a.y = a.y / sqrt(a.x*a.x + a.y*a.y + a.z *a.z);
+            a.z = a.z / sqrt(a.x*a.x + a.y*a.y + a.z *a.z);
+
+            point c1,c2,c3,i(1.0/sqrt(2.0),1.0/sqrt(2.0),0),j,k;
+
+
+            c1 = R(i,a,angle);
+            c2 = R(i,a,angle);
+            c3 = R(i,a,angle);
+
+
+
+
+            double** sc = create_2d_array();
+            sc[0][0] = c1.x;
+            sc[1][0] = c1.y;
+            sc[2][0] = c1.z;
+
+            sc[0][1] = c2.x;
+            sc[1][1] = c2.y;
+            sc[2][1] = c2.z;
+
+            sc[0][2] = c3.x;
+            sc[1][2] = c3.y;
+            sc[2][2] = c3.z;
+
+            sc[3][3] = 1;
+
+
+
+
+
+            double **temp = s.top();
+            double **ans = mul(temp,sc,4,4,4,4);
+            s.push(ans);
+
+
+
+
+
+
+
         }
         else if(input =="translate"){
 
@@ -310,6 +374,7 @@ int main(){
     fclose(stage1);
     //View Transformation
     stage1  = fopen("D:\\googleDrive\\_CSE\\Code\\Others\\Graphics\\Offline_2\\stage1.txt","r");
+    stage2 = fopen("D:\\googleDrive\\_CSE\\Code\\Others\\Graphics\\Offline_2\\stage2.txt","w");
     double** T = create_2d_array();
     T[0][3] = - eye.x;
     T[1][3] = -eye.y;
@@ -319,7 +384,7 @@ int main(){
     T[1][1] = 1;
     T[2][2] = 1;
     T[3][3] = 1;
-
+//http://www.sanfoundry.com/cpp-program-compute-cross-product-two-vectors/
     l.x = look_at.x - eye.x;
     l.y = look_at.y - eye.y;
     l.z = look_at.z - eye.z;
@@ -329,45 +394,64 @@ int main(){
     l.z = l.z / sqrt(l.x*l.x + l.y*l.y + l.z*l.z);
 
 
+    r.x = l.y * up.z - up.y * l.z;
+    r.y = up.x *l.y - l.x * up.z;
+    r.z =l.x * up.y - up.x * l.y;
+
+    r.x = r.x / sqrt(r.x*r.x + r.y*r.y + r.z*r.z);
+    r.y = r.y / sqrt(r.x*r.x + r.y*r.y + r.z*r.z);
+    r.z = r.z / sqrt(r.x*r.x + r.y*r.y + r.z*r.z);
+
+
+    u.x = r.y*l.z  - l.y * r.z;
+    u.y =  l.x  *r.y -r.x  * l.z;
+    u.z =r.x *l.y - l.x*r.y;
 
 
 
+    double** R = create_2d_array();
+    R[0][0] = r.x;
+    R[0][1] = r.y;
+    R[0][2] = r.z;
+
+    R[1][0] = u.x;
+    R[1][1]  = u.y;
+    R[1][2] = u.z;
+    R[2][0] = -l.x;
+    R[2][1] = -l.y;
+    R[2][2] = -l.z;
+    R[3][3] = 1;
+
+    double **V = mul(R,T,4,4,4,4);
+
+    double x, y, z;
+    int sum = 0;
+    //printf("dhukse");
+    while (fscanf(stage1, "%lf%lf%lf\n", &x, &y, &z) != EOF) {
+        //printf("-> --> %0.7f %0.7f %0.7f\n",x,y,z);
+       // printf("dhuksiii");
+
+        double **p = create_2d_array();
+        p[0][0] = x;
+        p[0][1] = y;
+        p[0][2] = z;
+        p[0][3] = 1;
+
+       double **ans = mul(V, p, 4, 4, 4, 4);
+
+        //printf("--------------------");
+        //print_matrix(ans,4,4);
+        fprintf(stage2, "%0.07f %0.07f %0.07f\n", ans[0][0], ans[1][0], ans[2][0]);
+
+        sum++;
+        if (sum % 3 == 0)fprintf(stage2, "\n");
 
 
 
-
-
-    /*
-
-    fp2 = fopen("D:\\googleDrive\\_CSE\\Code\\Others\\Graphics\\Offline_2\\out.txt","w");
-    fputs("successfully input taken",fp2);
-    fclose(fp2);
-
-
-    fp3  = fopen("D:\\googleDrive\\_CSE\\Code\\Others\\Graphics\\Offline_2\\out2.txt","w");
-    fprintf(fp3,"%d",(int)v.size());
-    fclose(fp3);
-    */
-
-
-/*
-    stage1  = fopen("D:\\googleDrive\\_CSE\\Code\\Others\\Graphics\\Offline_2\\stage1.txt","w");
-    //fprintf(stage1,"%d",(int)v.size());
-    //stage 1 processing should be here....
-
-    for(int i=0;i<(int)v.size();i++){
-        cout << v[i].ab.x << " " << v[i].ab.y << " " << v[i].ab.z << endl;
-        cout << v[i].bc.x << " " << v[i].bc.y << " " << v[i].bc.z << endl;
-        cout << v[i].ca.x << " " << v[i].ca.y << " " << v[i].ca.z << endl;
-        fprintf(stage1,"%0.7lf %0.7lf %0.7lf\n",v[i].ab.x,v[i].ab.y,v[i].ab.z);
-        fprintf(stage1,"%0.7lf %0.7lf %0.7lf\n",v[i].bc.x,v[i].bc.y,v[i].bc.z);
-        fprintf(stage1,"%0.7lf %0.7lf %0.7lf\n",v[i].ca.x,v[i].ca.y,v[i].ca.z);
-        //cout << endl;
-        fprintf(stage1,"\n");
     }
+    fclose(stage2);
 
-    fclose(stage1);
-    */
+
 
 
 
@@ -376,17 +460,13 @@ int main(){
     stage2  = fopen("D:\\googleDrive\\_CSE\\Code\\Others\\Graphics\\Offline_2\\stage2.txt","r");
     //stage 2 processing shoule be here...
     //fprintf(stage2,"%d",(int)v.size());
-
-
-
-
     stage3  = fopen("D:\\googleDrive\\_CSE\\Code\\Others\\Graphics\\Offline_2\\stage3.txt","w");
 
     //fprintf(fp3,"%d",(int)v.size());
    // printf("asheni ");
 
-        double x, y, z;
-        int sum = 0;
+        //double x, y, z;
+         sum = 0;
         while (fscanf(stage2, "%lf%lf%lf\n", &x, &y, &z) != EOF) {
             //printf("-> %0.7f %0.7f %0.7f\n",x,y,z);
             double **p = create_2d_array();
@@ -408,7 +488,7 @@ int main(){
             projection_matrix[2][3] = -(2.0 * far * near) / (far - near);
             projection_matrix[3][2] = -1.0;
 
-            double **ans = mul(p, projection_matrix, 4, 4, 4, 4);
+            double **ans = mul(projection_matrix, p, 4, 4, 4, 4);
             fprintf(stage3, "%0.07f %0.07f %0.07f\n", ans[0][0], ans[1][0], ans[2][0]);
 
             sum++;

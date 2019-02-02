5314bf3b0b23492261ae9474e2293722d7d497f6
= <forhadmethun@gmail.com>
2019-02-02 23:45:13 +0600

    amazon: Binarytree to DLL, Continuous Subarry with given Sum ,Find Four element sum equal to given sum, middle of linked list, if root to leaf path sum equal to some number eixist (2 Feb, 2019) -fm

---
+++ interviews/src/amazon/BinaryTreeLeavesToDLL.java
@@ -0,0 +1,59 @@
+package amazon;
+
+public class BinaryTreeLeavesToDLL {
+    static class Node{
+        int data;
+        Node prev;
+        Node next;
+        Node(int data){this.data = data;}
+    }
+    public static void main(String[] args) {
+        Node root = new Node(7);
+        Node l = new Node(4);
+        Node ll = new Node(7);
+        Node r = new Node(5);
+        Node lr = new Node(8);
+
+        root.prev = l;
+        l.prev = ll;
+
+        l.next = lr;
+
+        root.next = r;
+
+        Node head = allTreeNodeToDLL(root);
+
+        printDLL(head);
+
+    }
+
+    private static void printDLL(Node head) {
+        Node tempNode = head;
+        while(tempNode!=null){
+            System.out.println(tempNode.data);
+            tempNode = tempNode.prev;
+        }
+    }
+
+    private static Node allTreeNodeToDLL(Node root) {
+        if(root == null) return root;
+        if(root.prev == null && root.next == null){
+            return createNewNode(root.data);
+        }
+
+        Node tempLeft = allTreeNodeToDLL(root.prev);
+        Node tempRight = allTreeNodeToDLL(root.next);
+
+        if(tempLeft == null) return tempRight;
+        if(tempRight == null) return tempLeft;
+
+        tempLeft.next = tempRight;
+        tempRight.prev = tempLeft;
+
+        return tempRight;
+    }
+
+    private static Node createNewNode(int data) {
+        return new Node(data);
+    }
+}
---
+++ interviews/src/amazon/FindMiddleOfLinkedList.java
@@ -0,0 +1,51 @@
+package amazon;
+
+public class FindMiddleOfLinkedList {
+    static class Node{
+        int data;
+        Node next;
+        Node (int data){
+            this.data = data;
+        }
+    }
+
+    static void print(Node head){
+        Node tempNode = head;
+        while(tempNode!=null){
+            System.out.printf(" %d",tempNode.data);
+            tempNode = tempNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        Node child1 = new Node(2);
+        Node child2 = new Node(3);
+        Node child3 = new Node( 4);
+        Node child4 = new Node(5);
+
+        head.next = child1;
+        child1.next = child2;
+//        child2.next = child3;
+//        child3.next = child4;
+
+//        print(head);
+        int middle = middleOfLinkedList(head);
+        System.out.println(middle);
+    }
+
+    // I didn't check few corner cases
+    // Those cases should be checked
+    // for example, if the list contains only one element or two element
+    // those cases should be checked!!
+    private static int middleOfLinkedList(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null){
+            if(fast.next == null) break;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
+}

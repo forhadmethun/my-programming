307760a527281a7a94ac251e660c3fc7000a45d1
= <forhadmethun@gmail.com>
2019-05-14 22:01:33 +0600

    java file enhanced. :)

--- Others/general-topics/misc-concept/string-manipulation/src/StringManipulator.java
+++ Others/general-topics/misc-concept/string-manipulation/src/StringManipulator.java
@@ -7,60 +7,28 @@ class StringManipulator {
     static String inputPath = "/media/forhad/Development/MyCodes/Code/Others/general-topics/misc-concept/string-manipulation/src/files/input";
     static String outputPath = "/media/forhad/Development/MyCodes/Code/Others/general-topics/misc-concept/string-manipulation/src/files/output";
     public static void main(String[] args) throws Exception {
-
         Collection<File> fileCollection = listFileTree(new File(inputPath));
         for (File file : fileCollection) {
             if(file.getAbsolutePath().endsWith(".csv"))
             handleStringManipulation(file);
-//            System.out.println(file.getAbsolutePath());
         }
-//        handleStringManipulation();
-
-//        System.out.println(lines.get(0));
     }
 
     private static void handleStringManipulation(File file) throws Exception{
-
         String path = file.getAbsolutePath();
-
         BufferedReader reader = new BufferedReader(new FileReader(path));
-//        BufferedReader reader = new BufferedReader(new FileReader("/media/forhad/Development/MyCodes/Code/Others/general-topics/misc-concept/string-manipulation/src/train-combined-0.99.csv"));
-
-
-        String ANGER = "Anger";
-        String ANTICIPATION = "Anticipation";
-        String DISGUST = "Disgust";
-        String SURPRISE = "Surprise";
-        String JOY = "Joy";
-        String FEAR = "Fear";
-        String SADNESS = "Sadness";
-        String TRUST = "Trust";
-
-        ArrayList<String> emoticons = new ArrayList<>();
-        emoticons.add(ANGER);
-        emoticons.add(ANTICIPATION);
-        emoticons.add(DISGUST);
-        emoticons.add(SURPRISE);
-        emoticons.add(JOY);
-        emoticons.add(FEAR);
-        emoticons.add(SADNESS);
-        emoticons.add(TRUST);
-
+        String[] emoticonsArray = {"Anger", "Anticipation","Disgust","Surprise","Joy","Fear", "Sadness","Trust"};
+        ArrayList<String> emoticons = new ArrayList<>(Arrays.asList(emoticonsArray));
         List<String> lines = new ArrayList<>();
-
         Map<String, ArrayList<String>> stringArrayListMap = new HashMap<>();
         emoticons.forEach(e->{
             stringArrayListMap.put(e,new ArrayList<>());
         });
-
         String line = null;
         while ((line = reader.readLine()) != null) {
             for(String e: emoticons){
-//            emoticons.forEach(e->{
                 populateMapData(stringArrayListMap,e,line,emoticons);
             }
-//            );
-//            lines.add((line.replace("\t","")).replace("-",""));
         }
         printMapData(stringArrayListMap,outputPath+"/"+file.getName().replace(".csv","-" + "out.txt"));
     }
@@ -70,7 +38,6 @@ class StringManipulator {
         fw  = new FileWriter(path);
         stringArrayListMap.keySet().forEach(k->{
             stringArrayListMap.get(k).forEach(e->{
-//                System.out.println(k + " : " + e);
                 try {
                     fw.write(k+ " : " + e+"\n");
                 } catch (IOException ex) {

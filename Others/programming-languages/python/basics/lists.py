a70417a9d3ded264ebe98758dcc8138ebfe4397d
= <=forhadmethun@gmail.com>
2019-09-05 00:39:02 +0300

    python list methods

--- Others/programming-languages/python/basics/lists.py
+++ Others/programming-languages/python/basics/lists.py
@@ -17,6 +17,7 @@ if "apple" in thelist:
 #thelist.insert(1,"orange")
 #thelist.remove("banana")
 #thelist.pop()
+# pop(pos) - here position is optional...
 #del thelist
 #thelist.clear()
 #thelist = list((list items))
@@ -35,3 +36,36 @@ print("========================")
 print(thelist.index("cherry"))
 # thelist.
 
+
+def my_func(e):
+    return len(e)
+def list_methods_test():
+    cars = [ "forad", "bmw", "volvo"]
+    #sort method
+    #list.sort(reverse = True|False, key = myFunc)
+    cars.sort()
+    cars.sort(reverse = True, key = my_func)
+    print(cars)
+
+    #reverse method
+    cars.reverse() 
+
+    #extend an array with annother array
+    oldcars = ["zeep", "oldzeep"]
+    cars.extend(oldcars)
+    #can be extend with tuple
+
+    #count - how many times a value exists
+    print(cars.count("zeep"))
+
+    #copy one array to another value
+    my_facinating_cars = cars.copy()
+
+
+
+
+
+    
+
+# if __name__ == "__main__":
+list_methods_test()
